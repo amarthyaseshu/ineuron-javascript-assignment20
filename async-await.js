7b2466bf154caab83335e42c3b1283cc6942fb8e
amarthyaseshu <amarthyaseshu2@gmail.com>
2022-11-10 22:29:54 +0530

    [async await solved 5/10]

---
+++ async-await.js
@@ -0,0 +1,33 @@
+/*
+1. Async/Await internally uses promises, promises uses asynchronous programming where asynchronous tasks 
+are executed in call back queue & then moved to call stack. Due to this asynchronous nature performance increases.
+*/
+
+/* 4 Output:
+3
+*/
+
+/* 5 Output:
+1
+Error: some error
+*/
+
+/* 6 Output:
+3
+2
+1
+Go!
+*/
+
+/* 7 Output:
+15
+*/
+
+/* 10 Output:
+
+->When the processing is simple, less date & not complex
+->When there's a single connection to db
+->When one resource execution completely requires, previous resource output to process
+
+
+*/
